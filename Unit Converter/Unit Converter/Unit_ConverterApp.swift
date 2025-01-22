17f567b51b5bdab3924cb2ff8d6d75545191addb
NickKhachtryan <nickoarm@mail.ru>
2025-01-22 15:55:46 +0300

    Initial commit

---
+++ Unit Converter/Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Nick Khachatryan on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
