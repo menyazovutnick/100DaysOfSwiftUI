b1500fe50a06f3912cd71bf980437ac31fe8142c
NickKhachtryan <nickoarm@mail.ru>
2025-01-23 18:25:16 +0300

    challenge 1 is done

--- Unit Converter/Unit Converter/ContentView.swift
+++ Unit Converter/Unit Converter/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var lengthOptions: [String] = ["Meters", "Kilometers", "Feet", "Yards", "Miles"]
+    private var lengthUnits: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
+    
+    @State private var selectedInputUnit = 0
+    @State private var selectedOutputUnit = 0
+
+    @State private var inputValue = 0.0
+    
+    private var convertedValue: Double {
+        let value = inputValue
+        let convertedValue = Measurement(value: value, unit: lengthUnits[selectedInputUnit])
+        return convertedValue.converted(to: lengthUnits[selectedOutputUnit]).value
+        
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Form {
+                
+                Section {
+                    TextField("Amount", value: $inputValue, format: .number)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Unit", selection: $selectedInputUnit) {
+                        ForEach(0..<lengthOptions.count) {
+                            Text("\(lengthOptions[$0])")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text("\(convertedValue)")
+                    
+                    Picker("Unit", selection: $selectedOutputUnit) {
+                        ForEach(0..<lengthOptions.count) {
+                            Text("\(lengthOptions[$0])")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
         }
-        .padding()
     }
 }
 
