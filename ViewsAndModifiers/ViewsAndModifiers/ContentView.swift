3e1ba9197ec6738c933c9d8ad44ede51f45c1424
NickKhachtryan <nickoarm@mail.ru>
2025-01-24 19:32:42 +0300

    added ViewsAndModifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,30 +7,25 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<columns, id: \.self) { column in
-                        content(row, column)
-                    }
-                }
-                
-            }
-        }
+struct LargeTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func largeTitle() -> some View {
+        self.modifier(LargeTitle())
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Custom ViewModifier")
+            .largeTitle()
     }
-
 }
 
 #Preview {
