8269053f290917c25c7108ecc137342380b6e9af
NickKhachtryan <nickoarm@mail.ru>
2025-02-20 02:00:58 +0300

    iExpense app added

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nick Khachatryan on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
