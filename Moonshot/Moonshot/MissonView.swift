fddd96eea91985c5a05dbf8c34d718b9ad3a395b
NickKhachtryan <nickoarm@mail.ru>
2025-02-25 01:19:25 +0300

    mission view added

---
+++ Moonshot/Moonshot/MissonView.swift
@@ -0,0 +1,45 @@
+//
+//  MissonView.swift
+//  Moonshot
+//
+//  Created by Nick Khachatryan on 25.02.2025.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
